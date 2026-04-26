1c0d5cb1514f4163196c4655cfe3d5adcfd703ea
Hanna Toivola <p64947@edu.sasky.fi>
2026-04-26 21:22:42 +0300

    lisää AppRouter-komponentin ja reitityksen

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,34 +1,10 @@
-import Settings from '../../pages/Settings'
-
-import Stats from '../../pages/Stats'
-
-import Items from '../../pages/Items'
-
-import { ButtonContainer, FloatingButton } from '../../shared/ui/buttons'
-
-import Button from '../../shared/ui/buttons'
-
-import Content from '../Content'
-
-import Menu from '../Menu'
-
-import Header from '../Header'
-
-import styles from './App.module.scss'
+import AppRouter from '../../router/AppRouter'
 
 function App() {
 
-    return (
+  return (
     <>
-      <ButtonContainer>
-      <div className={styles.app}>
-        <Header />
-        <Content>
-        <Settings />
-        </Content>
-        <Menu />
-      </div>
-      </ButtonContainer>
+      <AppRouter />
     </>
   )
 }
