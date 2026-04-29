29c613a60c1f456ccf1c7700072ecf19c2827699
Hanna Toivola <p64947@edu.sasky.fi>
2026-04-29 19:35:40 +0300

    lisää päivämäärämuotoilun Item-komponentille

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -4,8 +4,15 @@ import styles from './Item.module.scss'
 
 function Item({data, ...props}) {
 
+    // Käytettävä lokaaliasetus päivämäärän muotoiluun.
+  const locale = "fi-FI"
+
+  // Muodostetaan maksupäivä Date-oliosta ja muotoillaan se käyttäjän
+  // lokaalin mukaan.
+  const paymentDate = new Date(data.paymentDate).toLocaleDateString(locale)
+
  return (
-    <div className={styles.item}>
+   <div className={styles.item_date}>{paymentDate}</div>
       <div className={styles.item_data}>
         <div className={styles.item_type}>{data.type}</div>
         <div className={styles.item_amount}>{data.amount} €</div>
